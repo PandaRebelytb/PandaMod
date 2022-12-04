c88f36fcaaec0c3393e938ce20677f37eeea65ce
PandaRebelyt <nathanclaes.pro@gmail.com>
2022-12-04 20:52:40 +0100

    fix block

--- src/main/java/fr/PandaRebel4307/PandaMod/PandaMod.java
+++ src/main/java/fr/PandaRebel4307/PandaMod/PandaMod.java
@@ -2,27 +2,12 @@ package fr.PandaRebel4307.PandaMod;
 
 import com.mojang.logging.LogUtils;
 import fr.PandaRebel4307.PandaMod.block.PandaBlock;
-import item.pandaitem;
-import net.minecraft.client.Minecraft;
-import net.minecraft.world.item.BlockItem;
-import net.minecraft.world.item.CreativeModeTab;
-import net.minecraft.world.item.Item;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.block.state.BlockBehaviour;
-import net.minecraft.world.level.material.Material;
-import net.minecraftforge.api.distmarker.Dist;
+import fr.PandaRebel4307.PandaMod.item.pandaitem;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import net.minecraftforge.registries.DeferredRegister;
-import net.minecraftforge.registries.ForgeRegistries;
-import net.minecraftforge.registries.RegistryObject;
 import org.slf4j.Logger;
 
 
--- src/main/java/fr/PandaRebel4307/PandaMod/item/pandaitem.java
+++ src/main/java/fr/PandaRebel4307/PandaMod/item/pandaitem.java
@@ -1,4 +1,4 @@
-package item;
+package fr.PandaRebel4307.PandaMod.item;
 
 import fr.PandaRebel4307.PandaMod.PandaMod;
 import net.minecraft.world.item.CreativeModeTab;
@@ -11,7 +11,7 @@ import net.minecraftforge.registries.RegistryObject;
 public class pandaitem {
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, PandaMod.MODID);
 
-    public static final RegistryObject<Item> halfinum_ingot = ITEMS.register ("halfinum_ingot", () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_TOOLS)));
+    public static final RegistryObject<Item> halfinum_ingot = ITEMS.register("halfinum_ingot", () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_TOOLS)));
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
     }
